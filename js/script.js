156c49bd1c47b21a53a7bdac2dbd2fa7846a5242
Andrew DeNike <denikeandrew@gmail.com>
2018-07-28 14:18:10 -0700

    Reassigned class defaultOpen

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function openCity(evt, cityName) {
 } 
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+document.getElementsByClassName("defaultOpen")[0].click();
 
 
 

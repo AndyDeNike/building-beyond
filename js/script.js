0b37b51d8b151a1f2c77346d4b92425179e0fe5e
Andrew DeNike <denikeandrew@gmail.com>
2018-07-28 17:50:54 -0700

    Created hideContent() encapsulating openCity() former code / added document.getElementById(s)

--- js/script.js
+++ js/script.js
@@ -1,33 +1,62 @@
-function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
-    // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-} 
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementsByClassName("defaultOpen")[0].click();
+//document.getElementsByClassName("defaultOpen")[0].click();
 
 
-
-
+//hideContent()
+//document.getElementById('Los Gatos Hills, CA').style.display = "block";
+//document.getElementById('Los Gatos Hills, CA').className += " active";
 
 var slideIndex = 1;
 showSlides(slideIndex);
+hideContent() 
+document.getElementById('Palo Alto, CA').style.display = "block";
+document.getElementById('Palo Alto, CA').className += " active";
+
+//PALOALTO
+document.getElementById('paloAlto').onclick = function(){
+  showSlides(slideIndex=1)
+  
+  hideContent()
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById('Palo Alto, CA').style.display = "block";
+  document.getElementById('Palo Alto, CA').className += " active";
+}
+
+//LOSGATOS
+document.getElementById('losGatos').onclick = function(){
+  showSlides(slideIndex=4)
+
+  hideContent()
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById('Los Gatos Hills, CA').style.display = "block";
+  document.getElementById('Los Gatos Hills, CA').className += " active";
+}
+
+
+
+
+
+
+function hideContent(){
+  //Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+}
 
 // Next/previous controls
 function plusSlides(n) {

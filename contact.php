d7ad2bce5dc6ebcf56c22adeeefda5ccace54146
Andydenike <denikeandrew@gmail.com>
2018-12-18 14:02:36 -0800

    Changed meta for phone view head

--- contact.php
+++ contact.php
@@ -51,6 +51,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 	<link rel="shortcut icon" type="image/png" href="images/bbfavicon.png"/>
 	<link href="./css/bbwp.css" rel="stylesheet" type="text/css">
 	<title>Building & Beyond</title>

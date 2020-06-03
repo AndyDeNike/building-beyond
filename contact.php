47d13a9819f02fdf875e1c81801c9ce9555f37a5
Andydenike <denikeandrew@gmail.com>
2020-06-02 18:17:06 -0700

    Updated favicon / appletouch

--- contact.php
+++ contact.php
@@ -1,76 +1,74 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+	ini_set('display_errors',1); 
+	error_reporting(E_ALL);
 
-$msg = "";
-
-use PHPMailer\PHPMailer\PHPMailer;
-
-include_once "PHPMailer/PHPMailer.php";
-include_once "PHPMailer/SMTP.php";
-include_once "PHPMailer/Exception.php";
+	$msg = "";
+	use PHPMailer\PHPMailer\PHPMailer;
+	include_once "PHPMailer/PHPMailer.php";
+	include_once "PHPMailer/SMTP.php";
+	include_once "PHPMailer/Exception.php";
 
 
-if (isset($_POST['submit'])) {
+	if (isset($_POST['submit'])) {
+        
+        $honeypot = trim($_POST["email"]);
+        
+        if(!empty($honeypot)) {
+            exit;
+        }
+        
+		$name = $_POST['name'];
+		$email = $_POST['email_real'];
+		$phone = $_POST['phone'];
+		$message = $_POST['message'];
 
-	$honeypot = trim($_POST["email"]);
-
-	if (!empty($honeypot)) {
-		exit;
-	}
-
-	$name = $_POST['name'];
-	$email = $_POST['email_real'];
-	$phone = $_POST['phone'];
-	$message = $_POST['message'];
-
-	$mail = new PHPMailer();
+		$mail = new PHPMailer();
 
 
-	$mail->IsSMTP();
-	$mail->SMTPDebug = 0;  //1- errors/message 2- messages only
-	$mail->Host = 'smtp.gmail.com';
-	$mail->SMTPAuth = true;
-	$mail->Username = 'denikeandrew@gmail.com';
-	$mail->Password = 'yeawenkmvvqblblq';
-	$mail->SMTPSecure = 'tls';
-	$mail->Port = 587;
+		$mail->IsSMTP();
+		$mail->SMTPDebug = 0;  //1- errors/message 2- messages only
+		$mail->Host = 'smtp.gmail.com';
+		$mail->SMTPAuth = true;
+		$mail->Username = 'denikeandrew@gmail.com';
+		$mail->Password = 'yeawenkmvvqblblq';
+		$mail->SMTPSecure = 'tls'; 
+		$mail->Port = 587;
 
-	$mail->Subject = "Contact request from Buildingandbeyond.com";
-	$mail->isHTML(true);
-	$mail->addAddress('denikeandrew@gmail.com');
-	$mail->addAddress('ddenike@buildingandbeyond.com');
-	// $mail->addAddress('bbruce@buildingandbeyond.com');
-	$mail->addAddress('ehannan@buildingandbeyond.com');
-
-	$mail->setFrom($email);
-	$mail->Body = 'Name: ' . $name .
-		"<br><br>" . 'Email: ' . $email .
+		$mail->Subject = "Contact request from Buildingandbeyond.com";
+		$mail->isHTML(true);
+		$mail->addAddress('denikeandrew@gmail.com');
+		$mail->addAddress('ddenike@buildingandbeyond.com');
+		// $mail->addAddress('bbruce@buildingandbeyond.com');
+		$mail->addAddress('ehannan@buildingandbeyond.com');
+	
+		$mail->setFrom($email);
+		$mail->Body = 'Name: ' . $name . 
+		"<br><br>" . 'Email: ' . $email . 
 		"<br><br>" . 'Phone: ' . $phone .
 		"<br><br>" . 'Message: ' . $message;
 
-	if ($mail->send())
-		$msg = "Your email has been sent, thank you!";
+		if ($mail->send())
+			$msg = "Your email has been sent, thank you!";
 
-	else
-		$msg = "Please enter all contact fields below and try again!";
-}
+		else 
+			$msg = "Please enter all contact fields below and try again!";
+
+	}
 ?>
 
 
 <!DOCTYPE html>
 
 <html>
-
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta property="og:image" content="images/BB White Logo.jpg" />
-	<link rel="shortcut icon" type="image/png" href="images/bbfavicon.png" />
+	<meta property="og:image" content="images/BB White Logo.jpg"/> 
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="shortcut icon" type="image/png" href="images/bbfavicon.png"/>
 	<link href="./css/bbwp.css" rel="stylesheet" type="text/css">
 	<title>Building & Beyond</title>
 </head>
-
 <body>
 
 	<header>
@@ -82,55 +80,55 @@ if (isset($_POST['submit'])) {
 				<li class="current"><a href="contact">Contact</a></li>
 			</ul>
 		</nav>
-
+		
 	</header>
 
 	<section id="contactInfo" class="form-style-1">
 		<!-- <div class="middleContainer"> -->
-		<div id="contactForm">
-			<?php if ($msg != "") echo "<div style='color:#25673F;'>" . "$msg<br><br>" . "</div>"; ?>
-			<h1>Contact Us</h1>
-			<p>Building & Beyond, Inc.<br>
-				1610 Dell Avenue, Suite M<br>
-				Campbell, CA 95008<br>
-				CA License #706491</p>
+			<div id="contactForm">
+                <?php if ($msg != "") echo "<div style='color:#25673F;'>". "$msg<br><br>" ."</div>"; ?>
+				<h1>Contact Us</h1>
+				<p>Building & Beyond, Inc.<br>
+				   1610 Dell Avenue, Suite M<br>
+				   Campbell, CA 95008<br>
+				   CA License #706491</p>
 
-			<p>Phone: 408.379.4757<br>
-				Fax: 408.379.4760</p>
-		</div>
-
-
-		<div id="emailform">
-			<h1>Email Us</h1>
-			<form method="post" action="contact.php">
-				<div>
-					<label>Name</label><br>
-					<input name="name" type="text" placeholder="Name">
-				</div>
-				<div>
-					<input name="email" type="text" style="display: none;">
-				</div>
-				<div>
-					<label>Email</label><br>
-					<input name="email_real" type="email" placeholder="Email Address">
-				</div>
-				<div>
-					<label>Phone</label><br>
-					<input name="phone" type="phone" placeholder="Phone Number">
-				</div>
-				<div>
-					<label>Message</label><br>
-					<textarea name="message" placeholder="Message"></textarea>
-				</div>
-				<input type="submit" name="submit" value="Submit">
-				<!--<button class="button_1" type="submit">Send</button> -->
-			</form>
+				<p>Phone: 408.379.4757<br>
+				   Fax: 408.379.4760</p>
+			</div>
 
 
+			<div id="emailform">
+				<h1>Email Us</h1>
+				<form method="post" action="contact.php">
+					<div>
+						<label>Name</label><br>
+						<input name="name" type="text" placeholder="Name">
+						</div>
+                    <div>
+                        <input name = "email" type="text" style="display: none;">
+                    </div>
+					<div>
+						<label>Email</label><br>
+						<input name = "email_real" type="email" placeholder="Email Address">
+					</div>
+					<div>
+						<label>Phone</label><br>
+						<input name="phone" type="phone" placeholder="Phone Number">
+					</div>
+					<div>
+						<label>Message</label><br>
+						<textarea name="message" placeholder="Message"></textarea>
+					</div>
+					<input type="submit" name="submit" value="Submit">
+					<!--<button class="button_1" type="submit">Send</button> -->
+				</form>
 
-		</div>
 
 
+			</div>
+
+			
 		<!-- </div>	 -->
 	</section>
 
@@ -141,8 +139,7 @@ if (isset($_POST['submit'])) {
 		<p>Buildingandbeyond.com, Copyright &copy; CA License #706491</p>
 	</footer>
 
-	<script src="js/script.js"></script>
+<script src="js/script.js"></script>
 
 </body>
-
 </html>
